b46648e9d54d36cbc14b2d765f00ae61173f0b71
Jason Sinn <jrs341@me.com>
2016-09-15 13:25:15 -0500

    images are initally displayed still clicking works to play and pause pointed data to original still and fixed height

--- javascript/giftastic.js
+++ javascript/giftastic.js
@@ -14,7 +14,7 @@ function retrieveImages(){
 
 		var t = $(this).data('topic');
 
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + t + "&api_key=dc6zaTOxFJmzC&limit=10";
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=funny+" + t + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 		$.ajax({url: queryURL, method: 'GET'})
 		 .done(function(response) {
@@ -25,20 +25,40 @@ function retrieveImages(){
 		     for (var i = 0; i < results.length; i++) {
 		     	
 		     	var topicImage = $('<img>');
-		     	topicImage.attr('src', results[i].images.fixed_height.url);
+		     	topicImage.attr('src',results[i].images.original_still.url);
+		     	topicImage.attr('data-still', results[i].images.original_still.url);
+		     	topicImage.attr('data-animate', results[i].images.fixed_height.url);
+		     	topicImage.attr('data-state', 'still');
+		     	topicImage.addClass('topicImage');
 
 		     	$('#gif').append(topicImage);
-
 
 		     }
 		}); 
 	})
 }
 
+function changeState(){
+	$(document).on('click', '.topicImage', function(){
+		var state = $(this).attr('data-state');
+		if (state == 'still'){
+			$(this).attr('src', $(this).data('animate'));
+			$(this).attr('data-state', 'animate');
+		} else {
+			$(this).attr('src', $(this).data('still'));
+			$(this).attr('data-state', 'still');
+		}
+	});
+}
+
 $(document).ready(function(){
 
 	buttons();
 
+	changeState();
+
 	retrieveImages();
+
+	
 
 });
