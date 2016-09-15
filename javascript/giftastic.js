6b3b06c474e6b9d91b42bf81c1cd4f6a3e7f1e57
Jason Sinn <jrs341@me.com>
2016-09-15 11:49:08 -0500

    videos are appending to the gif div

--- javascript/giftastic.js
+++ javascript/giftastic.js
@@ -12,11 +12,25 @@ function buttons(){
 function retrieveImages(){
 	$('button').on('click', function(){
 
-		var queryURL = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+		var t = $(this).data('topic');
+
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + t + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 		$.ajax({url: queryURL, method: 'GET'})
 		 .done(function(response) {
 		     console.log(response);
+
+		     var results = response.data;
+
+		     for (var i = 0; i < results.length; i++) {
+		     	
+		     	var topicImage = $('<img>');
+		     	topicImage.attr('src', results[i].images.fixed_height.url);
+
+		     	$('#gif').append(topicImage);
+
+
+		     }
 		}); 
 	})
 }
