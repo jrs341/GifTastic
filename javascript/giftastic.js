75e4813f60f83ff5657ddc161fa8374e2c03d597
Jason Sinn <jrs341@me.com>
2016-09-15 11:10:26 -0500

    buttons are appending to the page with for loop

--- javascript/giftastic.js
+++ javascript/giftastic.js
@@ -1,3 +1,16 @@
+var topicArray = ['Dogs', 'Cats', 'Kids', 'Stunts'];
+
+function buttons(){
+	for (var i = 0; i < topicArray.length; i++) {
+		var b = $('<button>');
+		b.text(topicArray[i]);
+		$('#buttons').append(b);
+	}
+};
+
+
 $(document).ready(function(){
-	
+
+	buttons();
+
 });
