fcba38b46acf88a8f96ee069fecc454e65a3065f
Jason Sinn <jrs341@me.com>
2016-09-15 11:20:52 -0500

    added function retrieveImages button click is working and returning 25 images need to fix URL to accept button name

--- javascript/giftastic.js
+++ javascript/giftastic.js
@@ -8,9 +8,22 @@ function buttons(){
 	}
 };
 
+function retrieveImages(){
+	$('button').on('click', function(){
+
+		var queryURL = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+
+		$.ajax({url: queryURL, method: 'GET'})
+		 .done(function(response) {
+		     console.log(response);
+		}); 
+	})
+}
 
 $(document).ready(function(){
 
 	buttons();
+
+	retrieveImages();
 
 });
