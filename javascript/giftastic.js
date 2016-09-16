47863ca8c521cefe46d0f3314df77605cd310d05
Jason Sinn <jrs341@me.com>
2016-09-16 07:32:42 -0500

    trying to add images to a bootstrap carousel

--- javascript/giftastic.js
+++ javascript/giftastic.js
@@ -1,6 +1,9 @@
 var topicArray = ['Dogs', 'Cats', 'Kids', 'Stunts'];
 
 function buttons(){
+
+	$('#buttons').empty();
+
 	for (var i = 0; i < topicArray.length; i++) {
 		var b = $('<button>');
 		b.text(topicArray[i]);
@@ -13,7 +16,7 @@ function retrieveImages(){
 	$('button').on('click', function(){
 
 		$('#gif').empty();
-		
+
 		var t = $(this).data('topic');
 
 		var queryURL = "http://api.giphy.com/v1/gifs/search?q=funny+" + t + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -25,19 +28,21 @@ function retrieveImages(){
 		     var results = response.data;
 
 		     for (var i = 0; i < results.length; i++) {
-		     	
 		     	var topicImage = $('<img>');
-		     	topicImage.attr('src',results[i].images.original_still.url);
-		     	topicImage.attr('data-still', results[i].images.original_still.url);
-		     	topicImage.attr('data-animate', results[i].images.fixed_height.url);
-		     	topicImage.attr('data-state', 'still');
-		     	topicImage.addClass('topicImage');
-
-		     	$('#gif').append(topicImage);
-
+			     	topicImage.attr('src',results[i].images.original_still.url);
+			     	topicImage.attr('data-still', results[i].images.original_still.url);
+			     	topicImage.attr('data-animate', results[i].images.fixed_height.url);
+			     	topicImage.attr('data-state', 'still');
+			     	topicImage.addClass('topicImage');
+			     // var imageDiv = $('<div>');
+		     		// imageDiv.addClass('.display');
+			    // $('.carousel-inner').append(imageDiv);
+		     	$('.carousel-inner').append(topicImage);	
 		     }
 		}); 
 	})
+
+	addTopic();
 }
 
 function changeState(){
@@ -53,6 +58,21 @@ function changeState(){
 	});
 }
 
+function addTopic(){
+	$('#addTopic').on('click', function() {
+
+		var topic = $('#topicInput').val().trim();
+
+		topicArray.push(topic);
+
+		buttons();
+
+		retrieveImages();
+
+		return false;
+	});
+}
+
 $(document).ready(function(){
 
 	buttons();
@@ -60,6 +80,8 @@ $(document).ready(function(){
 	changeState();
 
 	retrieveImages();
+
+
 
 	
 
